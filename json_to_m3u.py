9db36acdc6cfe0fdc99041a4fc6ca3cc4c011c3b
TarunG78 <livetv_tarun@tvstar.com>
2025-07-09 20:44:43 +0100

    Update json_to_m3u.py

--- json_to_m3u.py
+++ json_to_m3u.py
@@ -1,23 +1,16 @@
 import json
 
 def json_to_m3u(json_file, m3u_file):
-    with open(json_file, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-    channels_by_group = data.get("channels", {})
+    with open(json_file, "r") as f:
+        channels = json.load(f)
 
-    with open(m3u_file, 'w', encoding='utf-8') as f:
-        f.write('#EXTM3U\n')
-        for group, channels in channels_by_group.items():
-            for ch in channels:
-                name = ch.get("name", "")
-                logo = ch.get("logo", "")
-                group_title = ch.get("group", group)
-                url = ch.get("url", "")
-                extinf = (
-                    f'#EXTINF:-1 tvg-logo="{logo}" group-title="{group_title}",{name}\n'
-                )
-                f.write(extinf)
-                f.write(f'{url}\n')
+    with open(m3u_file, "w") as m3u:
+        m3u.write("#EXTM3U\n")
+        for channel in channels:
+            m3u.write(
+                f'#EXTINF:-1 tvg-id="{channel["id"]}" tvg-name="{channel["name"]}" tvg-logo="{channel["logo"]}" group-title="{channel["group"]}",{channel["name"]}\n'
+                f'{channel["url"]}\n'
+            )
 
 if __name__ == "__main__":
     json_to_m3u('channels.json', 'playlist.m3u')
