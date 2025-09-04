3abca5df870df4395b5d9fcf4994bc20858e2e7a
TarunG78 <livetv_tarun@tvstar.com>
2025-09-04 11:17:09 +0100

    Update extraer_m3u_Shu_Version2.py

--- extraer_m3u_Shu_Version2.py
+++ extraer_m3u_Shu_Version2.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import shutil
 
 # Leer la URL del enlace M3U desde la variable de entorno
 url = os.getenv('URL_FILE_LINK')
@@ -12,9 +13,21 @@ try:
     response = requests.get(url)
     if response.status_code == 200:
         m3u_content = response.text
-        with open('lista_varios.m3u', 'w') as file:
+        filename = 'lista_varios.m3u'
+        with open(filename, 'w') as file:
             file.write(m3u_content)
-        print("La lista_varios.m3u ha sido creada con éxito.")
+        print(f"El archivo {filename} ha sido creado con éxito.")
+
+        # Carpeta de exportación
+        export_folder = 'export'
+        if not os.path.exists(export_folder):
+            os.makedirs(export_folder)
+            print(f"Carpeta '{export_folder}' creada.")
+
+        # Mover archivo a carpeta de exportación
+        export_path = os.path.join(export_folder, filename)
+        shutil.move(filename, export_path)
+        print(f"El archivo ha sido exportado a: {export_path}")
     else:
         print(f"Error al hacer la solicitud: {response.status_code}")
 except Exception as e:
